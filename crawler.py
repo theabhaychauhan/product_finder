31ed3f531148bd01061a2794a88184560fa020d5
Abhay Chauhan <abhaychauhan@Abhays-MacBook-Air.local>
2025-01-11 16:07:56 +0530

    Minor

--- crawler.py
+++ crawler.py
@@ -54,6 +54,8 @@ class Crawler:
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        
         chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
         chrome_options.add_argument(f"user-agent={random.choice(user_agents)}")
         self.driver = webdriver.Chrome(
